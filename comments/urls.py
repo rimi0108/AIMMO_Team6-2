d05c163fe72d5d4636048b5565303444d9badfb0
Hyerim Choi <hyerimc858@gmail.com>
2021-11-02 18:44:23 +0900

    Feat: 추가 댓글 CRUD 기능

--- comments/urls.py
+++ comments/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
 
+from .views import CommentView
+
+urlpatterns = [
+    path('<int:post_id>/comment', CommentView.as_view()),
+    path('<int:post_id>/comments', CommentView.as_view()),
+    path('<int:post_id>/comment/<int:comment_id>', CommentView.as_view()),
+    ]
--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ from .views import PostView, PostListView
 urlpatterns = [
     path('', PostView.as_view()),
     path('/<int:post_id>', PostView.as_view()),
-    path('/list', PostListView.as_view())
+    path('/list', PostListView.as_view()),
     ]
